00b1da8432dcfa8311c291d3b832d3d63caf8a5c
Paweł Grabas <pgrabas@gmail.com>
2019-05-12 20:55:18 +0200

    update

--- host/lib/rest-ota.lua
+++ host/lib/rest-ota.lua
@@ -3,25 +3,26 @@ local json = require("json")
 
 local ota_file = "host/lib/ota-service.lua"
 
-server:add_resource("ota", {
-   {
-      method = "GET",
-      path = "{[A-Z0-9]+}/timestamp",
-      produces = "application/json",
-      handler = function(_, id)
-         print("OTA", id .. "/timestamp")
-         return InvokeFile(ota_file, "Timestamp", id)
-      end,
-   },
-   {
-      method = "GET",
-      path = "{[A-Z0-9]+}/image",
-      produces = "text/plain",
-      handler = function(_, id)
-         print("OTA", id .. "/image")
-         return InvokeFile(ota_file, "Image", id)
-      end,
-   },
-})
+server:add_resource( "ota",{
+        {
+            method = "GET",
+            path = "{[A-Z0-9]+}/timestamp",
+            produces = "application/json",
+            handler = function(_, id)
+                print("OTA", id .. "/timestamp")
+                return InvokeFile(ota_file, "Timestamp", id)
+            end
+        },
+        {
+            method = "GET",
+            path = "{[A-Z0-9]+}/image",
+            produces = "text/plain",
+            handler = function(_, id)
+                print("OTA", id .. "/image")
+                return InvokeFile(ota_file, "Image", id)
+            end
+        }
+    }
+)
 
 print("Registered OTA service")
