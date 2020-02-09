b20af9e7ab470e1f777761ef8d2915df92efbdd7
Paweł Grabas <pgrabas@gmail.com>
2020-02-09 11:07:21 +0100

    early support for irx

--- src/srv/srv-irx.lua
+++ src/srv/srv-irx.lua
@@ -1,35 +1,34 @@
+local Module = {}
+Module.__index = Module
 
+function Module:ResetPulse(level, t)
+    print(string.format("VALUE 0x%08x",  self.value))
+    -- gpio.write(3, gpio.LOW)
 
-gpio.mode(3, gpio.OUTPUT)
-gpio.write(3, gpio.LOW)
+    print(sjson.encode(self.pulses))
+    self.pulses = { }
+    self:SetTrigger()
 
-pin = 4
-gpio.mode(pin, gpio.INT)
-
-pulse = tmr.now()
-bitno = 0
-value = 0
-
-function reset_pulse(level, t)
-    print(string.format("VALUE 0x%08x",  value))
-    gpio.write(3, gpio.LOW)
-    gpio.trig(pin, "up", start_pulse)
-
-    if bit.band(value, 0x00ff0000) ~= 0x00ff0000 then
+    if bit.band(self.value, 0x00f00000) ~= 0x00f00000 then
         print "invalid"
         return
     end
 
-    local code = bit.rshift(bit.band(value, 0xFF00), 8)
-    local negcode = bit.band(bit.bnot(value), 0xFF)
-    print(string.format("%x %x", code, negcode))
+    local code = bit.rshift(bit.band(self.value, 0xFF00), 8)
+    local negcode = bit.band(bit.bnot(self.value), 0xFF)
+    -- print(string.format("%x %x", code, negcode))
     if code == negcode then
-        print("CODE", code)
+        self.last_code = code
+        print(string.format("IRX: Got code %02x", code))
+        -- if Event then
+            -- Event("irx.code", { code = code })
+        -- end
     end
 end
 
-function bittrig(level, t)
-    local d = bit.band((t - pulse), 0x7fffffff)
+function Module:BitPulse(level, t)
+    local d = bit.band((t - self.pulse), 0x7fffffff)
+    table.insert(self.pulses, d)
     local b
     if d > 1000 and d < 1200 then
         b = 0
@@ -37,54 +36,92 @@ function bittrig(level, t)
         b = 1
     end    
 
-    if bitno >= 0 then
+    if self.bitno >= 0 then
         if b == 1 then
-            local bvalue = bit.lshift(1, bitno)
-            value = bit.bor(value, bvalue)
+            local bvalue = bit.lshift(1, self.bitno)
+            self.value = bit.bor(self.value, bvalue)
         end
     else
-        if bitno < -1 then
-            gpio.trig(pin, "up", reset_pulse)
+        if self.bitno < -1 then
+            gpio.trig(self.pin, "up", function(...) self:ResetPulse(...) end)
         end
     end
-    bitno = bitno-1
-    
-    pulse = t
+    self.bitno = self.bitno - 1
+    self.pulse = t
 end
 
-function space_pulse(level, t)
-    local d = bit.band((t - pulse), 0x7fffffff)
---    print("start", d)
+function Module:SpacePulse(level, t)
+    local d = bit.band((t - self.pulse), 0x7fffffff)
+    -- print("SpacePulse", d, level)
+    table.insert(self.pulses, d)
     if level == 1 then
         if d < 8 * 1000 then
             --abort
         end
     else
         if d > 4 * 1000 and d < 5 * 1000 then
-            gpio.trig(pin, "down", bittrig)
-            bitno = 31
-            value = 0
+            gpio.trig(self.pin, "down", function(...) self:BitPulse(...) end)
+            self.bitno = 31
+            self.value = 0
         else
             --abort
             if d > 2100 and d < 2300 then
                 --repeat code?
-                print "repeat"
-                gpio.write(3, gpio.LOW)
+                if self.last_code ~= nil
+                --  and Event 
+                 then
+                    print(string.format("IRX: Repeat code %02x", self.last_code))
+                    -- Event("irx.code", { code = self.last_code, is_repeat = true })
+                end
+                -- gpio.write(3, gpio.LOW)
             end
         end
     end
-    pulse = t    
+    self.pulse = t    
 end
 
-function start_pulse(level, t)
-    print "start"
-    gpio.trig(pin, "both", space_pulse)
-    bitno = 31
-    value = 0
-    pulse = t
-    gpio.write(3, gpio.HIGH)
+function Module:StartPulse(level, t)
+    -- print "start"
+    self.pulse = t
+    gpio.trig(self.pin, "both", function(...) self:SpacePulse(...) end)
+    -- gpio.write(3, gpio.HIGH) --?
 end
 
-node.setcpufreq(node.CPU160MHZ)
-gpio.trig(pin, "down", start_pulse)
+function Module:SetTrigger()
+    self.bitno = 31
+    self.value = 0
+    self.pulse = 0
+    self.pulses = { }
+    gpio.trig(self.pin, "down", function(...) self:StartPulse(...) end)
+end
+  
+function Module:ClearTrigger()
+-- gpio.trig(self.sd0)
+end
+  
+function Module:Init()
+    gpio.mode(self.pin, gpio.INT)
+    self:SetTrigger()
+end
 
+Module.EventHandlers = {
+    -- ["app.init.post-services"] = Module.Init,
+    ["app.start"] = Module.Init,
+    -- ["controller.init"] = Module.ContrllerInit,
+    -- ["ota.start"] = Module.ClearTrigger,
+}
+
+return {
+    Init = function()
+        if not hw or not hw.irx or not bit then
+            print("IRX: Preconditions not met")
+            return
+        end
+  
+        local conf = hw.irx
+        hw.irx = nil
+        
+        return setmetatable(conf, Module)
+    end,
+}
+  
