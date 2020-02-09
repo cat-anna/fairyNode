a850c2ea0dc74e34fae8f3dc0da632e8733005d9
Paweł Grabas <pgrabas@gmail.com>
2020-02-09 11:07:41 +0100

    add event command

--- src/srv/cmd/srv-command.lua
+++ src/srv/cmd/srv-command.lua
@@ -1,7 +1,11 @@
 
 
-local function GetTopic()
+local function GetCommandTopic()
     return "homie/" .. (wifi.sta.gethostname() or string.format("%06x", node.chipid())) .. "/$cmd"
+end
+
+local function GetEventTopic()
+    return "homie/" .. (wifi.sta.gethostname() or string.format("%06x", node.chipid())) .. "/$event"
 end
 
 local Module = { }
@@ -32,9 +36,32 @@ function Module:MqttCommand(topic, payload)
     HandleCommand(payload, output)
 end
 
+function Module:MqttEvent(topic, payload)
+    local event_name =  nil
+    local valid_arg = nil
+    local arg = nil
+
+    local coma_pos = payload:find(",")
+    if coma_pos == nil then
+        event_name = payload
+    else
+        event_name = payload:sub(1, coma_pos-1)
+        arg_string = payload:sub(coma_pos+1)
+        valid,arg = pcall(sjson.decode, arg_string)
+        if not valid then
+            return
+        end
+    end
+
+    if Event then
+        Event(event_name, arg)
+    end
+end
+
 function Module:OnMqttConnected(event, mqtt)
     self.mqtt = mqtt
-    mqtt:Subscribe(GetTopic(), function(...) self:MqttCommand(...) end)
+    mqtt:Subscribe(GetCommandTopic(), function(...) self:MqttCommand(...) end)
+    mqtt:Subscribe(GetEventTopic(), function(...) self:MqttEvent(...) end)
 end
 
 function Module:OnMqttDisconnected()
