aee2d207e5a60f6cb11c20d839787959be3ab62c
Paweł Grabas <pgrabas@gmail.com>
2021-12-05 11:03:39 +0100

    light rework of host homie engine

--- host/lib/modules/homie-client.lua
+++ host/lib/modules/homie-client.lua
@@ -2,11 +2,104 @@ local modules = require("lib/modules")
 local socket = require("socket")
 local copas = require "copas"
 
+-------------------------------------------------------------------------------
+
+local function format_integer(v)
+    return string.format(math.floor(tonumber(v)))
+end
+
+local function tointeger(v)
+    return math.floor(tonumber(v))
+end
+
+local function toboolean(v)
+    local t = type(v)
+    if t == "string" then return v == "true" end
+    if t == "number" then return v > 0 end
+    if t == "boolean" then return v end
+    return v ~= nil
+end
+
+local function format_boolean(v)
+    return v and "true" or "false"
+end
+
+local DatatypeParser = {
+    boolean = { to_homie = format_boolean, from_homie = toboolean },
+    string = { to_homie = tostring, from_homie = tostring },
+    float = { to_homie = tostring, from_homie = tonumber },
+    integer = { to_homie = format_integer, from_homie = tointeger },
+}
+
+local function FromHomieValue(datatype, value)
+    local fmt = DatatypeParser[datatype]
+    assert(fmt)
+    return fmt.from_homie(value)
+end
+
+local function ToHomieValue(datatype, value)
+    local fmt = DatatypeParser[datatype]
+    assert(fmt)
+    return fmt.to_homie(value)
+end
+
+-------------------------------------------------------------------------------
+
 local NodeObject = {}
 NodeObject.__index = NodeObject
 
 function NodeObject:SetValue(property, value)
-    self.controller:PublishNodePropertyValue(self.name, property, value)
+    self.properties[property]:SetValue(value)
+end
+
+-------------------------------------------------------------------------------
+
+local PropertyMT = {}
+PropertyMT.__index = PropertyMT
+
+function PropertyMT:IsRetained()
+    return (self.retained ~= nil) and self.retained or self.controller.retain
+end
+
+function PropertyMT:SetValue(value)
+    self.timestamp = os.time()
+    if self.value and self:IsRetained() and self.value == value then
+        print(string.format("HOMIE: Skipping update %s - retained value not changed", self:GetFullId()))
+        return
+    end
+    self.value = value
+    self.controller:Publish(
+        self:GetValuePublishTopic(),
+        ToHomieValue(self.datatype, value),
+        self.retained
+    )
+end
+
+function PropertyMT:GetValuePublishTopic()
+    return self:GetTopic()
+end
+
+function PropertyMT:GetFullId()
+    return string.format("%s.%s", self.node.id, self.id)
+end
+
+function PropertyMT:GetTopic(sub_option)
+    if sub_option then
+        return string.format("/%s/%s/%s", self.node.id, self.id, sub_option)
+    else
+        return string.format("/%s/%s", self.node.id, self.id)
+    end
+end
+
+function PropertyMT:ImportValue(topic, payload)
+    print(string.format("HOMIE: Importing value %s.%s=%s", self.node.id, self.id, payload))
+    if not self.handler then
+        print("HOMIE: no handler for " .. topic)
+        return
+    end
+    self.value = FromHomieValue(self.datatype, payload)
+    self.timestamp = os.time()
+    self.handler:SetNodeValue(topic, payload, self.node.id, self.id, self.value)
 end
 
 -------------------------------------------------------------------------------
@@ -19,43 +112,17 @@ HomieClient.Deps = {
     timers = "event-timers",
 }
 
-local DatatypeParser = {
-    boolean = function(v)
-        local t = type(v)
-        if t == "string" then return v == "true" end
-        if t == "number" then return v > 0 end
-        if t == "boolean" then return v end
-        return v ~= nil
-    end,
-    string = tostring,
-    number = tonumber, --TODO
-    float = tonumber,
-    integer = function(v)
-        return math.floor(tonumber(v))
-    end,
-}
-
-local function FormatPropertyValue(datatype, value)
-    local fmt = DatatypeParser[datatype]
-
-    if fmt then
-        value = fmt(value)
-    end
-    return tostring(value)
-end
-
-local function DecodePropertyValue(datatype, value)
-    local fmt = DatatypeParser[datatype]
-    if fmt then
-        return fmt(value)
-    end
-    return tostring(value)
-end
-
--------------------------------------------------------------------------------
-
 function HomieClient:Publish(sub_topic, payload, retain)
-    self.mqtt:PublishMessage(self.base_topic .. sub_topic, tostring(payload), retain ~= nil and retain or self.retain)
+    local retain_flag = (retain ~= nil) and retain or self.retain
+    self.mqtt:PublishMessage(self.base_topic .. sub_topic, tostring(payload), retain_flag)
+end
+
+function HomieClient:BatchPublish(values, retain)
+    local retain_flag = (retain ~= nil) and retain or self.retain
+    for _,v in ipairs(values) do
+        local sub_topic, payload = table.unpack(v)
+        self.mqtt:PublishMessage(self.base_topic .. sub_topic, tostring(payload), retain_flag)
+    end
 end
 
 -------------------------------------------------------------------------------
@@ -68,7 +135,9 @@ function HomieClient:EnterInitState()
     self:Publish("/$implementation", "fairyNode")
     self:Publish("/$fw/name", "fairyNode")
 
-    self:Publish("/$fw/FairyNode/version", "0.0.3")
+    self:Publish("/$fw/FairyNode/version", "0.0.4")
+    self:Publish("/$fw/FairyNode/mode", "host")
+    self:Publish("/$fw/FairyNode/os", "linux")
 
     self:Publish("/$name", self.client_name)
 
@@ -165,46 +234,45 @@ function HomieClient:AddNode(node_name, node)
     --     }
     -- }
     -- ]]
-    local props = { }
-    for prop_name,values in pairs(node.properties or {}) do
-        table.insert(props, prop_name)
+
+    node.id = node_name
+
+    local prop_names = { }
+    node.properties = node.properties or {}
+    for prop_name,property in pairs(node.properties) do
+        table.insert(prop_names, prop_name)
+        property.id=prop_name
 
         -- print(string.format("HOMIE: %s.%s", node_name or "?", prop_name or "?"))
-        if values.handler then
-            local mqtt = self.mqtt
-            local handler = values.handler
-
+        if property.handler then
             local settable_topic = self:GetHomiePropertySetTopic(node_name, prop_name)
-            print("HOMIE: Settable addres:", settable_topic)
+            print("HOMIE: Settable address:", settable_topic)
 
-            self.mqtt:WatchTopic(self:WatchTopicId(settable_topic), function(topic, payload)
-                print(string.format("HOMIE: Importing value %s.%s=%s", node_name, prop_name, payload))
-                SafeCall(function()
-                    handler:SetNodeValue(topic, payload, node_name, prop_name, DecodePropertyValue(values.datatype, payload))
-                end)
-            end, settable_topic)
+            local function proxy_handler(topic, payload)
+                return property:ImportValue(topic, payload)
+            end
+            self.mqtt:WatchTopic(self:WatchTopicId(settable_topic), proxy_handler, settable_topic)
         end
 
-        self:PublishNodeProperty(node_name, prop_name, "$settable", values.handler ~= nil)
+        self:PublishNodeProperty(node_name, prop_name, "$settable", property.handler ~= nil)
 
-        values.handler = nil
-        values.retained = nil
-
-        for k,v in pairs(values or {}) do
+        for k,v in pairs(property or {}) do
             if k[1] ~= "_" then
                 self:PublishNodeProperty(node_name, prop_name, "$" .. k, v)
             end
         end
-        self:PublishNodeProperty(node_name, prop_name, "$retained", "true")
+        self:PublishNodeProperty(node_name, prop_name, "$retained", property.retained and "true" or "false")
+
+        property.controller = self
+        property.node = node
+        setmetatable(property, PropertyMT)
     end
 
     self:PublishNode(node_name, "$name", node.name)
-    self:PublishNode(node_name, "$properties", table.concat(props, ","))
+    self:PublishNode(node_name, "$properties", table.concat(prop_names, ","))
 
-    return setmetatable({
-        name = node_name,
-        controller = self,
-    }, NodeObject)
+    node.controller = self
+    return setmetatable(node, NodeObject)
 end
 
 -------------------------------------------------------------------------------
@@ -217,8 +285,8 @@ function HomieClient:AfterReload()
         self:EnterInitState()
     end
 
-    self.sensor_timer = self.timers:RegisterTimer("sensor-read", 60)
-    self.sensor_timer_slow = self.timers:RegisterTimer("sensor-read.slow", 10 * 60)
+    self.sensor_timer = self.timers:RegisterTimer("sensor.read", 60)
+    self.sensor_timer_slow = self.timers:RegisterTimer("sensor.read.slow", 10 * 60)
 end
 
 function HomieClient:Init()
