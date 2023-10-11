d006e926072682f767f3e4f4f4cceaf1a7dad159
Anna Grabas <4catanna@gmail.com>
2023-10-11 12:04:09 +0200

    host sensor thermal proto

---
+++ host/lib/classes/sensor/sensor-thermal-sys.lua
@@ -0,0 +1,102 @@
+local file = require "pl.file"
+local lfs = require "lfs"
+
+-------------------------------------------------------------------------------
+
+local Thermal = {}
+Thermal.__base = "base/property/local-property"
+Thermal.__deps = { }
+Thermal.__config = { }
+Thermal.__type = "class"
+
+-------------------------------------------------------------------------------
+
+function Thermal:Tag()
+    return "SysThermal"
+end
+
+-------------------------------------------------------------------------------
+
+function Thermal:InitThermalNode(client)
+    self.thermal_props = {}
+
+    if lfs.attributes("/sys/class/thermal") then
+        self.thermal_node = client:AddNode("thermal", {
+            ready = true,
+            name = "Temperatures",
+            properties = self.thermal_props
+        })
+    else
+        self.thermal_node = nil
+        print("SYSINFO: /sys/class/thermal does not exist")
+    end
+end
+
+function Thermal:ReadSensors()
+    if self.thermal_node then
+        for id, e in pairs(self.thermal_props) do
+            self.thermal_node:SetValue(id, string.format("%.1f", tonumber(file.read(e._read_path)) / 1000))
+        end
+    end
+end
+
+-------------------------------------------------------------------------------
+
+local function FindThermalSensors()
+    local thermal_props = {}
+-- sensors -j  ??
+    if lfs.attributes("/sys/class/thermal") then
+        for entry in lfs.dir("/sys/class/thermal") do
+            if entry:match("thermal_zone%d+") then
+                local base_path = "/sys/class/thermal/" .. entry
+                local e = {
+                    name = file.read(base_path .. "/type"),
+                    unit = "C",
+                    datatype = "float",
+                    read_path = base_path .. "/temp",
+                    scale = (1 / 1000),
+                }
+                table.insert(thermal_props, e)
+            end
+        end
+    end
+    return thermal_props
+end
+
+function Thermal.ProbeSensor(manager)
+    return nil
+    -- local sensors = FindThermalSensors()
+    -- if #sensors == 0 then
+    --     return
+    -- end
+
+        -- self.thermal_node = client:AddNode("thermal", {
+        --     ready = true,
+        --     name = "Temperatures",
+        --     properties = self.thermal_props
+        -- })
+
+    -- manager:RegisterSensor{
+    --     owner = self,
+
+    --     class = DaylightSensor,
+
+    --     name = "Daylight",
+    --     id = "daylight",
+    --     values = {
+    --         -- fast
+    --         sun_azimuth = { datatype = "float", name = "Sun azimuth", },
+    --         sun_altitude = { datatype = "float", name = "Sun altitude", },
+    --         -- slow
+    --         moon_phase = { datatype = "float", name = "Moon phase", },
+    --         moon_phase_fraction = { datatype = "float", name = "Moon phase fraction", },
+    --         moon_phase_angle = { datatype = "float", name = "Moon phase angle", },
+    --         moon_altitude = { datatype = "float", name = "Moon altitude", },
+    --         moon_azimuth = { datatype = "float", name = "Moon azimuth", },
+    --     }
+    -- }
+end
+
+-------------------------------------------------------------------------------
+
+return Thermal
---
+++ host/lib/tools/sensors-json-parse.lua
@@ -0,0 +1,75 @@
+local json = require "json"
+-- local tablex = require "pl.tablex"
+local file = require "pl.file"
+-- local pretty = require "pl.pretty"
+
+-------------------------------------------------------------------------------
+
+local M = {}
+
+-------------------------------------------------------------------------------
+
+local function SanitizeId(id)
+    return id:gsub("([%- ])", "_")
+end
+
+-------------------------------------------------------------------------------
+
+function M.ParseJsonOutput(text)
+    local data = json.decode(text)
+    if not data then
+        print("Failed to parse sensors json output")
+        return nil
+    end
+
+    local devices = { }
+
+    for dev_k,dev_v in pairs(data) do
+        local key = SanitizeId(dev_k)
+        local name = dev_v.Adapter
+
+        local values = { }
+
+        for prop_k,prop_v in pairs(dev_v) do
+            if type(prop_v) == "table" then
+                for k,v in pairs(prop_v) do
+                    local name = k:match("(.-)_input")
+                    if name then
+                        local id = SanitizeId(prop_k .. "_" .. name)
+                        values[id] = v
+                    end
+                end
+            end
+        end
+
+        table.insert(devices, {
+            id = key,
+            name = name,
+            values = values,
+            unit = "°C",
+            datatype = "float",
+        })
+    end
+
+    return devices
+end
+
+function M.ParseJsonFile(f)
+    return M.ParseJsonOutput(file.read(f))
+end
+
+function M.GetCurrentReadings()
+    local f = io.popen("sensors", "r")
+    if not f then
+        return
+    end
+
+    local output = f:read("*a")
+    io.close(f)
+
+    return M.ParseJsonOutput(output)
+end
+
+-------------------------------------------------------------------------------
+
+return M
