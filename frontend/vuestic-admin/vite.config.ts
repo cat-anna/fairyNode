152e7fdf0624e392d73a8337e6fb63e09bbad4a3
Anna Grabas <4catanna@gmail.com>
2023-10-20 20:50:20 +0200

    configure proxy to backend for dev

--- frontend/vuestic-admin/vite.config.ts
+++ frontend/vuestic-admin/vite.config.ts
@@ -7,6 +7,15 @@ import VueI18nPlugin from '@intlify/vite-plugin-vue-i18n'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [
     vue(),
     VueI18nPlugin({
