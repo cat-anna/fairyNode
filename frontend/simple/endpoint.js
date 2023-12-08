3fc08ea3ecf6c1eb8c26f53003a9245a39b3e8eb
Anna Grabas <4catanna@gmail.com>
2023-12-08 10:02:33 +0100

    reorganization - shuffle files around

--- frontend/simple/endpoint.js
+++
@@ -1,8 +0,0 @@
-
-
-function HomieDeleteDevice(device_name, async_response){
-    if (window.confirm("Do you really want to delete device " + device_name + " ?")) {
-        body = {}
-        QueryPost("/device_deprecated/" + device_name + "/delete", body, async_response)
-    }
-}
