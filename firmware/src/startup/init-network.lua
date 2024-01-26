9dd511ad644b07cc97286bbdd4b73dfdf4b2e0b8
Anna Grabas <4catanna@gmail.com>
2024-01-26 21:16:14 +0100

    firmware: refactor network init

--- firmware/src/startup/init-network.lua
+++ firmware/src/startup/init-network.lua
@@ -1,50 +1,59 @@
-print "INIT: Initializing network..."
+print("INIT: Initializing network...")
 
-if Event then Event("wifi.disconnected", T) end
+local wifi_connected = nil
+local function SetWifiState(value)
+    if wifi_connected == value then
+        return
+    end
+    wifi_connected = value
+
+    node.task.post(function()
+        require("sys-led").Set("wifi", value)
+    end)
+    if Event then
+        if wifi_connected then
+            Event("wifi.connected")
+        else
+            Event("wifi.disconnected")
+        end
+    end
+end
+
+SetWifiState(false)
 
 local hostname = require("sys-config").Read("hostname.cfg")
 if not hostname then
-    print "WiFi: No name config file"
+    print("WiFi: No name config file")
     local chipid = node.chipid()
     hostname = string.format("ESP-%X", chipid)
 else
     hostname = hostname:match("%w+")
 end
 
-print("WiFi: My name is '" .. hostname .. "'")
+print("WiFi: My name is", hostname, "'")
 
 local station_cfg = require("sys-config").JSON("wifi.cfg")
 if not station_cfg then
-    print "WiFi: Network is disabled"
+    print("WiFi: Network is disabled")
     wifi.setmode(wifi.NULLMODE)
 else
-    print("WiFi: Connecting to " .. station_cfg.ssid .. "...")
+    print("WiFi: Connecting to", station_cfg.ssid)
 
     wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
-        print("WiFi: got IP address: " .. T.IP)
-        node.task.post(function() if Event then Event("wifi.gotip") end end)
-        node.task.post(function() require("sys-led").Set("wifi", true) end)
+        print("WiFi: got IP address:", T.IP)
+        SetWifiState(true)
         if failsafe then
-            pcall(function() require("ota-core").Check(failsafe) end)
+            node.task.post(function()
+                pcall(function() require("ota-core").Check(failsafe) end)
+            end)
         end
-        -- tmr.create():alarm((failsafe and 1 or 60) * 1000, tmr.ALARM_SINGLE,
-        --   function(t)
-        --       t:unregister()
-
-        --   end)
     end)
     wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
         print("WiFi: connected SSID: " .. T.SSID .. " BSSID: " .. T.BSSID .. " channel: " .. T.channel)
-        node.task.post(function()
-            if Event then Event("wifi.connected") end
-        end)
     end)
     wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, function(T)
         print("WiFi: disconnected SSID: " .. T.SSID .. " BSSID: " .. T.BSSID .. " reason: " .. T.reason)
-        node.task.post(function()
-            if Event then Event("wifi.disconnected") end
-        end)
-        node.task.post(function() require("sys-led").Set("wifi", false) end)
+        SetWifiState(false)
     end)
 
     wifi.setmode(wifi.STATION)
